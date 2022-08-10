cb9745f30a9cb236c5cd6b0c0772a5b6f554c973
Tocsmatts <toochukwuezeobi@gmail.com>
2022-08-10 07:01:48 -0700

    Adding file shell_str.c

---
+++ shell_str.c
@@ -0,0 +1,106 @@
+#include "main.h"
+
+/**
+ * _strcat - tis function joins two strings
+ * @dest: a char
+ * @src: char pointer
+ * Return: a char
+ */
+char *_strcat(char *dest, const char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * *_strcpy -this function copies strings
+ * @dest: a char
+ * @src: a char
+ * Return: a cahr
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+	size_t a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a] = '\0';
+
+	return (dest);
+}
+/**
+ * _strcmp - this function compares two strings.
+ * @s1: string one
+ * @s2: string two
+ * Return: 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+		;
+
+	if (s1[i] > s2[i])
+		return (1);
+	if (s1[i] < s2[i])
+		return (-1);
+	return (0);
+}
+/**
+ * _strchr - this function locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: a char
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}
+/**
+ * _strspn - this function gets the length of a substring.
+ * @s: a char
+ * @accept: char popinter
+ * Return: int
+ */
+int _strspn(char *s, char *accept)
+{
+	int i, j, bool;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
+}
